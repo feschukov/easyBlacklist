275f319191177702962f858edf9e1a8a5466de6d
sergant210 <sergant210@bk.ru>
2018-09-02 18:27:38 +0300

    v1.2.0-pl

--- _build/resolvers/resolve.tables.php
+++ _build/resolvers/resolve.tables.php
@@ -1,5 +1,5 @@
 <?php
-
+/** @var xPDOObject $object */
 if ($object->xpdo) {
 	/* @var modX $modx */
 	$modx =& $object->xpdo;
@@ -11,8 +11,70 @@ if ($object->xpdo) {
             $modx =& $object->xpdo;
             $modelPath = $modx->getOption('ebl_core_path',null,$modx->getOption('core_path').'components/easyblacklist/').'model/';
             $modx->addPackage('easyblacklist',$modelPath);
-            $m = $modx->getManager();
-            $m->createObjectContainer('eblBlacklist');
+            $manager = $modx->getManager();
+
+            $objects = array();
+            $schemaFile = MODX_CORE_PATH . 'components/easyblacklist/model/schema/easyblacklist.mysql.schema.xml';
+            if (is_file($schemaFile)) {
+                $schema = new SimpleXMLElement($schemaFile, 0, true);
+                if (isset($schema->object)) {
+                    foreach ($schema->object as $obj) {
+                        $objects[] = (string)$obj['class'];
+                    }
+                }
+                unset($schema);
+            }
+            foreach ($objects as $tmp) {
+                $table = $modx->getTableName($tmp);
+                $sql = "SHOW TABLES LIKE '".trim($table,'`')."'";
+                $stmt = $modx->prepare($sql);
+                $newTable = true;
+                if ($stmt->execute() && $stmt->fetchAll()) {
+                    $newTable = false;
+                }
+                // If the table is just created
+                if ($newTable) {
+                    $manager->createObjectContainer($tmp);
+                } else {
+                    // If the table exists
+                    // 1. Operate with tables
+                    $tableFields = array();
+                    $c = $modx->prepare("SHOW COLUMNS IN {$modx->getTableName($tmp)}");
+                    $c->execute();
+                    while ($cl = $c->fetch(PDO::FETCH_ASSOC)) {
+                        $tableFields[$cl['Field']] = $cl['Field'];
+                    }
+                    foreach ($modx->getFields($tmp) as $field => $v) {
+                        if (in_array($field, $tableFields)) {
+                            unset($tableFields[$field]);
+                            $manager->alterField($tmp, $field);
+                        } else {
+                            $manager->addField($tmp, $field);
+                        }
+                    }
+                    foreach ($tableFields as $field) {
+                        $manager->removeField($tmp, $field);
+                    }
+                    // 2. Operate with indexes
+                    $indexes = array();
+                    $c = $modx->prepare("SHOW INDEX FROM {$modx->getTableName($tmp)}");
+                    $c->execute();
+                    while ($cl = $c->fetch(PDO::FETCH_ASSOC)) {
+                        $indexes[$cl['Key_name']] = $cl['Key_name'];
+                    }
+                    foreach ($modx->getIndexMeta($tmp) as $name => $meta) {
+                        if (in_array($name, $indexes)) {
+                            unset($indexes[$name]);
+                        } else {
+                            $manager->addIndex($tmp, $name);
+                        }
+                    }
+                    foreach ($indexes as $index) {
+                        $manager->removeIndex($tmp, $index);
+                    }
+                }
+            }
+        break;
 
 			break;
 
